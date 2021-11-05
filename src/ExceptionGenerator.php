336722e05ba98d67ea52075d1574b7361d927da2
labile <1a6i1e@gmail.com>
2021-11-05 18:59:11 +0300

    fix typo

--- src/ExceptionGenerator.php
+++ src/ExceptionGenerator.php
@@ -33,64 +33,64 @@ class ExceptionGenerator
         switch ($this->error_code) {
             case 0:
                 throw new Exceptions\VkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 1:
                 throw new Exceptions\UnknownErrorVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 5:
                 throw new Exceptions\AuthorizationFailedVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 6:
                 throw new Exceptions\TooManyRequestsVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 7:
                 throw new Exceptions\PermissionDeniedVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 9:
                 throw new Exceptions\TooMuchSimilarVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 10:
                 throw new Exceptions\InternalErrorVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 14:
                 throw new Exceptions\CaptchaRequiredVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 15:
                 throw new Exceptions\AccessDeniedVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 27:
                 throw new Exceptions\AccessTokenIsInvalidVkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
             case 100:
                 throw new Exceptions\MissingOrInvalidParametersException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
 
             default:
                 throw new Exceptions\VkException(
-                    $this->error_code,
-                    $this->message
+                    $this->message,
+                    $this->error_code
                 );
         }
     }
